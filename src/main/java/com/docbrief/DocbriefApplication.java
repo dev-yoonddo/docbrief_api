19b1c611a2ace053cc250263680d6d6a39cfff56
yoonseo <oooo_0720@naver.com>
2026-01-12 22:21:28 +0900

    commit

---
+++ src/main/java/com/docbrief/DocbriefApplication.java
@@ -0,0 +1,15 @@
+package com.docbrief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DocbriefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocbriefApplication.class, args);
+        System.out.println("start application !!");
+	}
+
+}
